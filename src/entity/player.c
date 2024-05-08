9fca374bb858296a97dcfbbcb33e9b7d4ca0351c
justint9696 <justin.tonkinson96@gmail.com>
2024-05-07 22:03:13 -0600

    add player flags

--- src/entity/player.c
+++ src/entity/player.c
@@ -4,23 +4,17 @@
 #include "graphics/drawing.h"
 #include "world/world.h"
 
-#include <stdio.h>
-
-static void init(entity_s *self, world_s *world) {
-    printf("player initialized\n");
-}
-
 static void render(entity_s *self, world_s *world) {
     draw_quad(self->body.pos, self->body.dim, COLOR_BLUE);
 }
 
 entity_s *player_create(vec2s pos, vec2s dim, world_s *world) {
     return entity_create(&(entity_s) {
-        .init = init,
         .render = render,
         .body = {
             .pos = pos, 
             .dim = dim,
         },
+        .flags = FLAG_PLAYER_INPUT | FLAG_KINEMATIC,
     }, world);
 }
