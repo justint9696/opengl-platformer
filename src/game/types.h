340daffa5ed4bed03c60bef757a4c60d0ae99b87
justint9696 <justin.tonkinson96@gmail.com>
2024-05-07 22:04:55 -0600

    add movement direction

--- src/game/types.h
+++ src/game/types.h
@@ -6,6 +6,13 @@
 
 struct entity_s;
 
+typedef enum {
+    DIR_UP = 1 << 0,
+    DIR_DOWN = 1 << 1,
+    DIR_LEFT = 1 << 2,
+    DIR_RIGHT = 1 << 3,
+} direction_t;
+
 typedef struct {
     vec2s pos, dim;
 } Box;
@@ -17,7 +24,9 @@ typedef struct {
             vec2s pos, dim;
         };
     };
+    vec2s vel;
     bool grounded;
     time_t grounded_tick;
-    void (*collision_callback)(struct entity_s *, struct entity_s *);
-} RigidBody;
+    direction_t direction;
+    void (*collision_callback)(struct entity_s *, struct entity_s *, float dt);
+} rigid_body_s;
