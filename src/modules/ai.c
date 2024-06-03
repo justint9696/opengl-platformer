f26676cb76a69a0fad6ba9383aef82c6c34d3296
justint9696 <justin.tonkinson96@gmail.com>
2024-06-02 18:54:06 -0600

    renamed movement to direction

--- src/modules/ai.c
+++ src/modules/ai.c
@@ -4,10 +4,10 @@
 
 void ai_tick(entity_t *self, world_t *world, float dt) {
     if (self->body.pos.x <= -270) {
-        self->body.movement.x = 1.f; 
+        self->body.direction.x = 1.f; 
     } else if (self->body.pos.x >= 270 - self->body.dim.x) {
-        self->body.movement.x = -1.f; 
+        self->body.direction.x = -1.f; 
     } else if (self->body.vel.x == 0.f) {
-        self->body.movement.x = -1.f; 
+        self->body.direction.x = -1.f; 
     }
 }
