a2147e633694e63cf484b0b2b249cf474b281235
justint9696 <justin.tonkinson96@gmail.com>
2024-07-16 10:46:00 -0600

    added line numbers to logs

--- src/util/log.h
+++ src/util/log.h
@@ -4,14 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void _log_message(FILE *stream, const char *title, const char *file, int line, 
+                  const char *format, ...);
+
 #ifdef _DEBUG
-#define log_debug(...) fprintf(stdout, "DEBUG: " __VA_ARGS__);
+#define log_debug(...)\
+    _log_message(stdout, "DEBUG", __FILE__, __LINE__, __VA_ARGS__);
 #else
 #define log_debug(...)
 #endif // _DEBUG
 
-#define log_warn(...) fprintf(stdout, "WARNING: " __VA_ARGS__);
+#define log_warn(...)\
+    _log_message(stdout, "WARNING", __FILE__, __LINE__, __VA_ARGS__);
 
-#define log_and_fail(...) ({ fprintf(stderr, "ERROR: " __VA_ARGS__); exit(1); })
+#define log_and_fail(...) ({\
+        _log_message(stderr, "ERROR", __FILE__, __LINE__, __VA_ARGS__);\
+        exit(1);\
+    })
 
 #endif // _UTIL_LOG_H_
